c565293f84f333b22b93e0093b940357d0234cf4
Luiz Kim <luiz.kim@controleonline.com>
2024-07-23 20:37:47 -0300

    first commit

---
+++ src/router/routes.js
@@ -0,0 +1,18 @@
+export const routes = [
+  {
+    path: '/employee/',
+    component: () =>  import ('@controleonline/ui-layout/src/layouts/AdminLayout.vue'),
+    children: [
+      {
+        name: 'EmployeeIndex',
+        path: '',
+        component: () =>  import ('../pages/Employee/Index.vue')
+      },
+      {
+        name: 'EmployeesDetails',
+        path: 'id/:id',
+        component: () =>  import ('../pages/Employee/Details.vue')
+      },
+    ]
+  },
+];
