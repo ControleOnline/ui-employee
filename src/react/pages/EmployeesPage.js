daf3167748ca01522b9381baa20bca270cfb82b2
luizkim <luizkim@users.noreply.github.com>
2026-09-23 22:54:38 -0300

    chore(release): align dev with rc/1.10.31-rc.11

--- src/react/pages/EmployeesPage.js
+++ src/react/pages/EmployeesPage.js
@@ -1,3 +1,4 @@
+// fluxo: funcionario-cadastro | etapa: employees-page | wiki: https://github.com/ControleOnline/app-community/wiki/Venda-Producao
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import People from '@controleonline/ui-people/src/react/pages/People';
